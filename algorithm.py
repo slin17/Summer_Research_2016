2feceacd7909d2e4938558be3cf993d75e7b2808
Tino Zinyama <tzinyama@colgate.edu>
2016-06-22 14:31:20 -0400

    Added new score functions
    - one for node load
    - one for edge load
    - an update function to update loads

--- algorithm.py
+++ algorithm.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 import networkx as nx 
 import random
 
-#G = nx.fast_gnp_random_graph(15,0.5,5,False)
+#Global Variables
+node_loads = {}
+edge_loads = {}
+paths_used = {}
 
 def draw_graph(H):
 	labelsdict = {}
@@ -26,6 +29,7 @@ def removeDuplicate(set_of_paths_P):
 					retListofL.append(pathL)
 	return retListofL
 
+
 def get_all_edges_from_SOP(setP):
 	retL = []
 	for path in setP:
@@ -35,6 +39,7 @@ def get_all_edges_from_SOP(setP):
 			if not (temp in retL or tempRev in retL):
 				retL.append(temp)
 	return retL
+
 
 def scoreFunc(path, uncoveredL):
 	retScore = 0
@@ -47,6 +52,7 @@ def scoreFunc(path, uncoveredL):
 			retScore += 1
 	return retScore
 
+
 def deleteEdgesFromL(path, uncoveredL):
 	for i in xrange(len(path)-1):
 		temp = (path[i],path[i+1])
@@ -58,7 +64,13 @@ def deleteEdgesFromL(path, uncoveredL):
 		if bool2:
 			uncoveredL.remove(tempRev)
 
+
 def greedyAlgorithm(setP, uncoveredL):
+	#coeficients
+    EDGE_COVERAGE = 1
+    EDGE_LOAD = 0
+    NODE_LOAD = 0
+
 	retPaths = []
 	while len(uncoveredL) > 0:
 		maxScore = 0
@@ -71,12 +83,14 @@ def greedyAlgorithm(setP, uncoveredL):
 		retPaths.append(maxP)
 	return retPaths
 
+
 def evaluateGreedyResult(setPaths):
 	S = set()
 	for path in setPaths:
 		S.add(path[0])
 		S.add(path[len(path)-1])
 	return S
+
 
 def generate_graphs_params(filename):
 	param_graphs = []
@@ -90,11 +104,13 @@ def generate_graphs_params(filename):
 
 	return param_graphs
 
+
 def main(filename):
     #read graph creation properties from file
     g = generate_graphs_params(filename)
     G = nx.fast_gnp_random_graph(g[0], g[1], g[2], False)
     
+    #add edge weights 
     H = G.copy()
     randSeed = g[5]
     for (u,v) in H.edges():
@@ -102,12 +118,62 @@ def main(filename):
     	rInt = random.randint(g[3],g[4])
     	H[u][v]['w'] = rInt
     	randSeed += 1
+
+    #initialize all network loads to 1
+    nds = G.nodes();
+    for nd in nds:
+        node_loads[nd] = 1
+        
+    for e in uncovered_edges:
+        edge_loads[e] = 1
+
     draw_graph(H)
+
     set_of_paths_P = nx.all_pairs_shortest_path(H)
     setP = removeDuplicate(set_of_paths_P)
     uncoveredL = get_all_edges_from_SOP(setP)
     retPaths = greedyAlgorithm(setP, uncoveredL)
     print retPaths
+
+
+#*********************************************
+def node_load_score(path):
+    score = 0
+    for nd in path:
+        score += node_loads[nd]
+        
+    return score
+
+
+def edge_load_score(path):
+    score = 0
+    for i in range (len(path) - 1):
+        edge = (path[i], path[i + 1])
+        rev_edge = (path[i + 1], path[i])
+        
+        if edge in edge_loads:
+            score += edge_loads[edge]
+        elif rev_edge in edge_loads:
+            score += edge_loads[rev_edge]
+    
+    return score
+
+
+def update_load_values(path):
+    #update node loads
+    for nd in path:
+        node_loads[nd] += 1;
+        
+    #update edge loads
+    for i in range(len(path) - 1):
+        edge = (path[i], path[i + 1])
+        rev_edge = (path[i + 1], path[i])
+        
+        if edge in edge_loads:
+            edge_loads[edge] += 1
+        elif rev_edge in edge_loads:
+            edge_loads[rev_edge] += 1
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
